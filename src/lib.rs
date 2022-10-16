81119efd35193bd0ed487680fa5b0bc0594e5a7f
Vineet Palepu <vineetmpalepu@gmail.com>
2022-10-16 06:23:51 -0700

    added random expression generation as fallback if no expression provided as part of command line args

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod solver;
 mod tokens;
 
+use rand::{thread_rng, Rng};
 use solver::*;
 use tokens::*;
 
@@ -12,4 +13,47 @@ pub fn evaluate(expr: &str) -> f64
     let expr_tree = ExpressionTree::from_postfix_tokens(postfix_tokens);
 
     expr_tree.eval()
+}
+
+pub fn generate_expression(terms: usize) -> String
+{
+    let (min, max) = (0f32, 10f32);
+
+    let mut rng = thread_rng();
+    let num1 = format!("{:.2}", rng.gen_range(min..max));
+
+    let mut ops = Vec::new();
+    let mut nums = Vec::new();
+
+    fn get_rand_operator() -> String
+    {
+        let mut rng = thread_rng();
+        let op = match rng.gen_range(0..=3)
+        {
+            0 => "+",
+            1 => "-",
+            2 => "*",
+            3 => "/",
+            _ => panic!("rand num bounds error"),
+        };
+
+        op.to_string()
+    }
+
+    for i in 0..terms - 1
+    {
+        ops.push(get_rand_operator());
+        nums.push(rng.gen_range(min..max));
+    }
+
+    let mut expression = Vec::new();
+    expression.push(num1);
+
+    for i in 0..terms - 1
+    {
+        expression.push(ops[i].to_string());
+        expression.push(format!("{:.2}", nums[i]));
+    }
+
+    expression.into_iter().collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,48 @@
 use std::env;
 
+use math_evaluator::generate_expression;
+
 fn main()
 {
+    // MAIN
     let args: Vec<String> = env::args().collect();
-    let expr = &args[1];
+    let expr = if args.len() == 2
+    {
+        args[1].clone()
+    }
+    else
+    {
+        math_evaluator::generate_expression(1000)
+    };
+
     println!("{}", expr);
 
-    math_evaluator::evaluate(expr);
-    /*
-    //let expr = "0004^(0000.5/(3-.1)+2)-.2^.13-.23+23.22";
-    //let expr = "2 ^ (3 + 4)";
-    //let expr = "4+ 26/ (8- 2)^  4";
-    let expr = "2 + 4 + 6 + 8 + 10";
-    //let expr = "(4*2-(8-3))/(5^.4*2) + 5^(.2-.8/3)";
+    math_evaluator::evaluate(&expr);
 
-    math_evaluator::evaluate(expr);
+    // TESTING
+    /*
+    // let expr = "0004^(0000.5/(3-.1)+2)-.2^.13-.23+23.22";
+    // let expr = "2 ^ (3 + 4)";
+    // let expr = "4+ 26/ (8- 2)^  4";
+    // let expr = "2 + 4 + 6 + 8 + 10";
+    // let expr = "(4*2-(8-3))/(5^.4*2) + 5^(.2-.8/3)";
+    math_evaluator::evaluate(&expr);
     */
 }
+
+// TODO: implement
+//
+// multi-threaded solving of large expressions (on gpu?)
+//      find multiple leaf nodes at once and solve simultaneously
+//      or maybe find N (where N is the number of cores/threads) nodes in the expression tree where none are childen of each other and
+//      solve the subtrees independently
+//      if translating to gpu can maybe queue up operations and operands then push to gpu and evaluate
+//
+//
+// Formal syntax specification:
+//
+//
+// expression = number (bin_op number)?
+// expression = "(" expression ")"
+// number = "-"? digit+ ("."digit+)?
+// bin_op = "+" | "-" | "*" | "/"
